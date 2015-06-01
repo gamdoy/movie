0bdb7c6c10393565d646bbea45b710624b25be4f
seokyeonyoon <ksnilbog@naver.com>
2015-06-01 13:54:12 +0900

    달력이벤트관련 js 파일 추가

--- movie_project/WebContent/WEB-INF/view/stats/statistics_form.jsp
+++ movie_project/WebContent/WEB-INF/view/stats/statistics_form.jsp
@@ -1,11 +1,12 @@
 <%@ page contentType= "text/html;charset=UTF-8"%>
 <!DOCTYPE html>
 <html>
+<script type="text/javascript" src="<%= request.getContextPath()%>/WEB-INF/view/common/click_cal.js"></script>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
-
+<input type="text" OnClick="Calendar(this, 'down','no');">
 </body>
 </html>
--- movie_project/src/kr/or/kosta/stats/controller/StatsController.java
+++ movie_project/src/kr/or/kosta/stats/controller/StatsController.java
@@ -1,5 +1,13 @@
 package kr.or.kosta.stats.controller;
 
-public class StatsController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/stats/")
+public class StatsController {
+		@RequestMapping(value="cal")
+		public String testCal(){
+			return "stats/statistics_form.tiles";
+		}
 }
