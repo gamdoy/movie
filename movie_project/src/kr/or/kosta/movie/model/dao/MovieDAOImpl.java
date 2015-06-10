5c1ddb999dba04635252a1d32bcff811bf86f0c0
KOSTA_03_002 <KOSTA_03_002@KOSTA_03_002-HP>
2015-06-10 09:56:55 +0900

    리스트화면 페이징 작업중

--- movie_project/src/kr/or/kosta/movie/model/dao/MovieDAOImpl.java
+++ movie_project/src/kr/or/kosta/movie/model/dao/MovieDAOImpl.java
@@ -1,8 +1,11 @@
 package kr.or.kosta.movie.model.dao;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import kr.or.kosta.event.controller.PagingBean;
 import kr.or.kosta.movie.vo.ActorVO;
 import kr.or.kosta.movie.vo.DirectorVO;
 import kr.or.kosta.movie.vo.MovieVO;
@@ -21,48 +24,52 @@ public class MovieDAOImpl implements MovieDAO {
 	@Autowired
 	private SqlSessionTemplate session;
 	
-	
-	//영화 등록 메소드
+	//영화등록
 	@Override
 	public int insertMovie(MovieVO movie) {
-	
 		 return session.insert(namespace+"insertMovie",movie);
-		
 	}
-
+	//영화수정
 	@Override
 	public int updateMovie(MovieVO movie) {
 		return session.update(namespace+"updateMovie",movie);
-		
 	}
-
+	//movNo로 영화조회
 	@Override
 	public MovieVO getMovieByNo(String movNo) {
 		return session.selectOne(namespace+"getMovieByNo",movNo);
 	}
-	
+	//모든영화 조회-paging
 	@Override
-	public List<MovieVO> allMovieList() {
-		
-		return session.selectList(namespace+"allMovie");
+	public List<MovieVO> allMovieList(int pageNo) {
+		Map param = new HashMap();
+		param.put("contentsPerPage", PagingBean.CONTENTS_PER_PAGE);
+		param.put("pageNo", pageNo);
+		return session.selectList(namespace+"allMovie",pageNo);
 	}
-	
+	 
+	//모든영화 카운트
+	@Override
+	public int totalCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	//개별table 조회
 	@Override
 	public List<DirectorVO> getDirector() {
 		return session.selectList(namespace+"director");
 	}
-
 	@Override
 	public List<ActorVO> getActor() {
 		return session.selectList(namespace+"actor");
-		
 	}
-
 	@Override
 	public List<ProductionVO> getProduction() {
 		return session.selectList(namespace+"production");
 	}
 
+	
 	
 
 
--- movie_project/src/kr/or/kosta/movie/model/service/MovieServiceImpl.java
+++ movie_project/src/kr/or/kosta/movie/model/service/MovieServiceImpl.java
@@ -1,53 +1,64 @@
 package kr.or.kosta.movie.model.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import kr.or.kosta.event.controller.PagingBean;
 import kr.or.kosta.movie.model.dao.MovieDAO;
 import kr.or.kosta.movie.vo.ActorVO;
 import kr.or.kosta.movie.vo.DirectorVO;
 import kr.or.kosta.movie.vo.MovieVO;
 import kr.or.kosta.movie.vo.ProductionVO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 @Service
 public class MovieServiceImpl implements MovieService {
 	
 	@Autowired
 	private MovieDAO dao;
-
+	
+	//영화등록
 	@Override
 	public int registerMovie(MovieVO movie) {
 		return dao.insertMovie(movie);
 	}
-
+	//영화수정
 	@Override
 	public int updateMovie(MovieVO movie){
 		return dao.updateMovie(movie);
 	}
-	
+	//모든영화 조회-paging
 	@Override
-	public List<MovieVO> allMovieList() {
-		return dao.allMovieList();
+	public Map allMovieList(int pageNo) {
+		//목록에 뿌려줄 List<MovieVO> 조회
+		List<MovieVO> list = dao.allMovieList(pageNo);
+		int count = dao.totalCount();
+		//PagingBean 생성
+		PagingBean pagingBean = new PagingBean(count, pageNo);
+		//두개의 값(List, PagingBean)을 Map에 넣어 return
+		HashMap map = new HashMap();
+		map.put("movie", list);
+		map.put("pagingBean", pagingBean);
+		return map;
 	}
-
+	//movNo로 영화조회
+	@Override
+	public MovieVO getMovieByNo(String movNo) {
+		return dao.getMovieByNo(movNo);
+	}
+	
+	
+	//개별table 조회
 	@Override
 	public List<DirectorVO> getDirector() {
 		return dao.getDirector();
 	}
-	
-	@Override
-	public MovieVO getMovieByNo(String movNo) {
-		
-		return dao.getMovieByNo(movNo);
-	}
-
 	@Override
 	public List<ActorVO> getActor() {
-		
 		return dao.getActor();
 	}
-
 	@Override
 	public List<ProductionVO> getProduction() {
 		return dao.getProduction();
