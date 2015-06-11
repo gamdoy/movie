887c30b6ce3aedb79c4155af74e74dd378228c6f
arisongha <arisongha@naver.com>
2015-06-11 21:17:57 +0900

    notice DAO 패키지 추가

---
+++ movie_project/src/kr/or/kosta/notice/model/dao/NoticeDAO.java
@@ -0,0 +1,5 @@
+package kr.or.kosta.notice.model.dao;
+
+public interface NoticeDAO {
+
+}
---
+++ movie_project/src/kr/or/kosta/notice/model/dao/NoticeDAOImpl.java
@@ -0,0 +1,8 @@
+package kr.or.kosta.notice.model.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class NoticeDAOImpl implements NoticeDAO{
+
+}
